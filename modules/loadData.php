bccff969aa6e5d83348cad8de9d7d06eb9fb2f8d
Tobias Rees <Toby@resgear.home>
2016-06-03 18:24:16 +0200

    no message

--- modules/loadData.php
+++ modules/loadData.php
@@ -1,38 +1,10 @@
 <?php
 include ('../resources/eXist.php');
 
-
-
         $query = '
         for $record in doc('."'".'/db/apps/WineDBxml/resources/WeinDB.xml'."'".')//WeinDB/Wein
-        let $id := $record/@WeinID/string()
-        let $name := $record/Name/text()
-        let $herst := $record/Hersteller/text()
-        let $land := $record/Land/text()
-        let $region := $record/Region/text()
-        let $weinfarbe := $record/Weinfarbe/text()
-        let $sorte := $record/Traubensorte/text()
-        let $jahr := $record/Jahrgang/text()
-        let $anzahl := $record/Anzahl/text()
-        let $punkte := $record/Punkte/text()
-        let $ab := $record/TrinkenAb/text()
-        let $bis := $record/TrinkenBis/text()
-        return
-        <tr>
-            <td>{$id}</td>
-            <td><a href="#" class="editName" data-pk="{$id}">{$name}</a></td>
-            <td><a href="#" class="editHerst" data-pk="{$id}">{$herst}</a></td>
-            <td><a href="#" class="editLand" data-pk="{$id}">{$land}</a></td>
-            <td><a href="#" class="editRegion" data-pk="{$id}">{$region}</a></td>
-            <td><a href="#" class="editSelectColor" data-pk="{$id}">{$weinfarbe}</a></td>
-            <td class="sorteTD"><a href="#" class="editSorte" data-pk="{$id}">{$sorte}</a></td>
-            <td><a href="#" class="editJahr" data-pk="{$id}">{$jahr}</a></td>
-            <td><a href="#" class="editAnzahl" data-pk="{$id}">{$anzahl}</a></td>
-            <td><a href="#" class="editPunkte" data-pk="{$id}">{$punkte}</a></td>
-            <td><a href="#" class="editTrinkenAb" data-pk="{$id}">{$ab}</a></td>
-            <td><a href="#" class="editTrinkenBis" data-pk="{$id}">{$bis}</a></td>
-        </tr>
-        ';
+        return $record';
+
         // Schreibt den Inhalt in die Datei zurück
         $output;
         function queryDB($query) {
@@ -49,40 +21,27 @@ include ('../resources/eXist.php');
             if (!$answer) {
                 throw new Exception($db->getError());
             } 
-
             # Get results
+            $records = "<WeinDB>";
             if ( !empty($answer["XML"]) ) {
                     foreach ( $answer["XML"] as $xml) {
-                            $records .= $xml;
+                            $records .= $xml . "\n";
                         }
                     }
-
+            $records .= "</WeinDB>";
             if ($db->disconnect()) {
                 throw new Exception($db->getError());
             }
             return $records;
         } 
-
+        $records;
         try {
-            $output = queryDB($query);
+            $records = queryDB($query); 
         } catch (Exception $e) {
             echo "Error: " . $e->getMessage();
         }
-
-            $outputTest =   '<tr>
-            <td>{$id}</td>
-            <td><a href="#" class="editName" data-pk="{$id}">{$name}</a></td>
-            <td><a href="#" class="editHerst" data-pk="{$id}">{$herst}</a></td>
-            <td><a href="#" class="editLand" data-pk="{$id}">{$land}</a></td>
-            <td><a href="#" class="editRegion" data-pk="{$id}">{$region}</a></td>
-            <td><a href="#" class="editSelectColor" data-pk="{$id}">{$weinfarbe}</a></td>
-            <td class="sorteTD"><a href="#" class="editSorte" data-pk="{$id}">{$sorte}</a></td>
-            <td><a href="#" class="editJahr" data-pk="{$id}">{$jahr}</a></td>
-            <td><a href="#" class="editAnzahl" data-pk="{$id}">{$anzahl}</a></td>
-            <td><a href="#" class="editPunkte" data-pk="{$id}">{$punkte}</a></td>
-            <td><a href="#" class="editTrinkenAb" data-pk="{$id}">{$ab}</a></td>
-            <td><a href="#" class="editTrinkenBis" data-pk="{$id}">{$bis}</a></td>
-        </tr>';
+        $xmlLoad = simplexml_load_string($records);
+        $output = json_encode($xmlLoad);
         echo $output;
 
 ?>
